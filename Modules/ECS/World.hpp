6aeeead86d454092a4da56cb968bf1b7e600697d
JX-Master <jxmaster@yeah.net>
2023-02-23 12:39:02 +0800

    [ECS]Remove deprecated comments in `IWorld`.

--- Modules/ECS/World.hpp
+++ Modules/ECS/World.hpp
@@ -54,79 +54,9 @@ namespace Luna
 		{
 			luiid("{14F85B5E-D509-40A8-A7F6-49778783418A}");
 
-			//! Gets the entity record for the specified entity.
-			// virtual RV query_entity(entity_id_t id, EntityAddress* out_address) = 0;
-
-			// bool is_entity_valid(entity_id_t id)
-			// {
-			// 	return succeeded(query_entity(id, nullptr));
-			// }
-
-			// template <typename _Ty>
-			// bool has_component(entity_id_t id)
-			// {
-			// 	Cluster* cluster;
-			// 	usize index;
-			// 	auto r = query_entity(id, &cluster, &index);
-			// 	if (failed(r)) return r.errcode();
-			// 	typeinfo_t type = typeof<_Ty>();
-			// 	void* components = get_cluster_components_data(cluster, type);
-			// 	return components ? true : false;
-			// }
-
-			// template <typename _Ty>
-			// R<_Ty*> get_component(entity_id_t id)
-			// {
-			// 	Cluster* cluster;
-			// 	usize index;
-			// 	auto r = query_entity(id, &cluster, &index);
-			// 	if (failed(r)) return r.errcode();
-			// 	typeinfo_t type = typeof<_Ty>();
-			// 	void* components = get_cluster_components_data(cluster, type);
-			// 	if (!components) return ECSError::component_not_found();
-			// 	return (_Ty*)((usize)components + index * get_type_size(type));
-			// }
-
 			//! Gets the cluster by components and tags.
 			 virtual Cluster* get_cluster(Span<const typeinfo_t> components, Span<const entity_id_t> tags, 
 			 	bool create_if_not_exist = false) = 0;
-
-			// //! Adds one entity to the world. This modification is applied immediately.
-			// //! @return Returns the entity ID of the created entity.
-			// //! @remark This function can only be called when the caller has exclusive access to the world.
-			// virtual entity_id_t add_entity() = 0;
-
-			// //! Removes the specified entity from the world. This modification is applied immediately.
-			// //! @param[in] id The ID of the entity to remove.
-			// //! @remark If the entity does not exist when this operation is committed, this operation does nothing.
-			// //! This function can only be called when the caller has exclusive access to the world.
-			// virtual void remove_entity(entity_id_t id) = 0;
-
-			// //! Removes all entities in the world. This modification is applied immediately.
-			// //! @remark This function can only be called when the caller has exclusive access to the world.
-			// virtual void remove_all_entities() = 0;
-
-			// //! Adds and removes multiple components and tags by specifying the target cluster directly.
-			// //! @remark This function can only be called when the caller has exclusive access to the world.
-			// virtual void set_entity_cluster(const EntityAddress& src_address, 
-			// 	Cluster* dest_cluster,
-			// 	const HashMap<typeinfo_t, void*>& data,
-			// 	EntityAddress* out_address) = 0;
-
-			// //! Adds one task barrier to the processing queue.
-			// virtual JobSystem::job_id_t enqueue_task_barrier(TaskBarrierFlag flags) = 0;
-
-			// //! Adds one task to the processing queue.
-			// virtual	JobSystem::job_id_t enqueue_task(const TaskDesc& desc, task_func_t* task_func, void* params = nullptr) = 0;
-
-			// //! Begins a synchronized task on the current thread.
-			// //! @param[in] desc The task descriptor.
-			// //! @return Returns the job ID that should be passed to `end_task` when the task is finished.
-			// virtual JobSystem::job_id_t begin_task(const TaskDesc& desc) = 0;
-
-			// //! Finishes the task opend by `begin_task`.
-			// //! @param[in] task The job ID returned by `begin_task`.
-			// virtual void end_task(JobSystem::job_id_t task) = 0;
 		};
 
 		//! Creates one new world.
