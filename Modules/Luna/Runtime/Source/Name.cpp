0e6b7a95e0e3d1c500c4fb75497020773926f44f
JX-Master <jxmaster@yeah.net>
2023-11-04 11:53:16 +0800

    [Runtime]Change name system implementation to make it more friendly to multi-thread environment.

--- Modules/Luna/Runtime/Source/Name.cpp
+++ Modules/Luna/Runtime/Source/Name.cpp
@@ -18,111 +18,50 @@ namespace Luna
 {
 	struct NameEntry
 	{
-		NameEntry* m_cache_list_prior;
-		NameEntry* m_cache_list_next;
-		c8* m_str;
 		usize m_str_size;
 		name_id_t m_id;
 		u32 m_ref_count;
 
-		NameEntry(name_id_t id, c8* str, usize str_size, u32 ref_count) :
+		NameEntry(name_id_t id, usize str_size, u32 ref_count) :
 			m_id(id),
-			m_cache_list_prior(nullptr),
-			m_cache_list_next(nullptr),
-			m_str(str),
 			m_str_size(str_size),
 			m_ref_count(ref_count) {}
 	};
+	inline NameEntry* get_name_entry(const c8* name)
+	{
+		return ((NameEntry*)name) - 1;
+	}
+	inline const c8* get_name_string(NameEntry* entry)
+	{
+		return (const c8*)(entry + 1);
+	}
 	struct NameEntryExtractKey
 	{
-		const name_id_t operator()(const NameEntry& v)
+		const name_id_t operator()(NameEntry* v)
 		{
-			return v.m_id;
+			return v->m_id;
 		}
 	};
-	Unconstructed<SelfIndexedUnorderedMultiMap<name_id_t, NameEntry, NameEntryExtractKey>> g_name_map;
-	NameEntry* g_name_cache_list_head; // Caches names whose reference count is 0. These names will cache a short period of time to prevent being interned/released frequently.
-	NameEntry* g_name_cache_list_tail;
-	usize g_name_retain_list_size;
+	Unconstructed<SelfIndexedUnorderedMultiMap<name_id_t, NameEntry*, NameEntryExtractKey>> g_name_map;
 	SpinLock g_name_mtx;
 	bool g_name_inited = false;
-	static void add_name_to_cache_list(NameEntry* entry)
-	{
-		// Add to head.
-		if (g_name_cache_list_head)
-		{
-			g_name_cache_list_head->m_cache_list_prior = entry;
-		}
-		entry->m_cache_list_next = g_name_cache_list_head;
-		g_name_cache_list_head = entry;
-		if (!g_name_cache_list_tail) // If the list is empty...
-		{
-			g_name_cache_list_tail = entry;
-		}
-		++g_name_retain_list_size;
-	}
-	static void remove_name_from_cache_list(NameEntry* entry)
-	{
-		if (entry->m_cache_list_prior)
-		{
-			entry->m_cache_list_prior->m_cache_list_next = entry->m_cache_list_next;
-		}
-		if (entry->m_cache_list_next)
-		{
-			entry->m_cache_list_next->m_cache_list_prior = entry->m_cache_list_prior;
-		}
-		if (g_name_cache_list_head == entry)
-		{
-			g_name_cache_list_head = entry->m_cache_list_next;
-		}
-		if (g_name_cache_list_tail == entry)
-		{
-			g_name_cache_list_tail = entry->m_cache_list_prior;
-		}
-		entry->m_cache_list_next = nullptr;
-		entry->m_cache_list_prior = nullptr;
-		--g_name_retain_list_size;
-	}
 	static void erase_entry(NameEntry* entry)
 	{
-		memfree(((NameEntry**)(entry->m_str)) - 1);
 		auto range = g_name_map.get().equal_range(entry->m_id);
 		luassert(range.first != g_name_map.get().end());
 		for (auto iter = range.first; iter != range.second; ++iter)
 		{
-			if (entry == &(*iter))
+			if (entry == *iter)
 			{
 				g_name_map.get().erase(iter);
-				return;
+				break;
 			}
 		}
-		luassert(false);
-	}
-	static void name_cache_list_cleanup()
-	{
-		// TODO: Tweak these two values based on the real situation.
-		constexpr usize cleanup_threshold = 1000;				 // The maximum number of entries to cache.
-		constexpr usize max_cleanup_count_per_call = 50;		 // The maximum number of entries to free at each cache call.
-		if (g_name_retain_list_size <= cleanup_threshold) return;
-		usize cleanup_count = min(g_name_retain_list_size - cleanup_threshold, max_cleanup_count_per_call);
-		// Cleanup is performed from tail to head since unused names will bubble to tail as time goes by.
-		NameEntry* entry = g_name_cache_list_tail;
-		for(usize i = 0; i < cleanup_count; ++i)
-		{
-			NameEntry* next = entry->m_cache_list_prior;
-			erase_entry(entry);
-			entry = next;
-		}
-		entry->m_cache_list_next = nullptr;
-		g_name_cache_list_tail = entry;
-		g_name_retain_list_size -= cleanup_count;
+		memfree(entry);
 	}
 	void name_init()
 	{
 		g_name_map.construct();
-		g_name_cache_list_head = nullptr;
-		g_name_cache_list_tail = nullptr;
-		g_name_retain_list_size = 0;
 		g_name_inited = true;
 	}
 	void name_close()
@@ -130,7 +69,7 @@ namespace Luna
 		// Release all name strings.
 		for (auto& i : g_name_map.get())
 		{
-			memfree(((NameEntry**)(i.m_str)) - 1);
+			memfree(i);
 		}
 		g_name_map.destruct();
 		g_name_inited = false;
@@ -150,69 +89,58 @@ namespace Luna
 		auto range = g_name_map.get().equal_range(h);
 		if (range.first != g_name_map.get().end())
 		{
-			// Compare each string to find the right one.
-			for (auto iter = range.first; iter != range.second; ++iter)
+			auto iter = range.first;
+			// Skip comparison for single-string case.
+			++iter;
+			if(iter == range.second)
 			{
-				if (!memcmp(name, iter->m_str, count * sizeof(c8)))
+				iter = range.first;
+				NameEntry* entry = *iter;
+				atom_inc_u32(&(entry->m_ref_count));
+				return get_name_string(entry);
+			}
+			// Compare each string to find the right one.
+			for (iter = range.first; iter != range.second; ++iter)
+			{
+				NameEntry* entry = *iter;
+				const c8* entry_string = get_name_string(entry);
+				if (!memcmp(name, entry_string, count * sizeof(c8)))
 				{
-					if (!(iter->m_ref_count))
-					{
-						remove_name_from_cache_list(&(*iter));
-					}
-					++(iter->m_ref_count);
-					return iter->m_str;
+					atom_inc_u32(&(entry->m_ref_count));
+					return entry_string;
 				}
 			}
 		}
 		// Create new entry.
-		name_cache_list_cleanup();
-		NameEntry** name_buf = (NameEntry**)memalloc(sizeof(c8) * (count + 1) + sizeof(NameEntry**));
-		c8* buf = (c8*)(name_buf + 1);
+		NameEntry* new_entry = (NameEntry*)memalloc(sizeof(c8) * (count + 1) + sizeof(NameEntry), alignof(NameEntry));
+		new (new_entry) NameEntry(h, count, 1);
+		c8* buf = (c8*)(new_entry + 1);
 		memcpy(buf, name, sizeof(c8) * count);
 		buf[count] = 0;
-		auto iter = g_name_map.get().insert(NameEntry(h, buf, count, 1));
-		*name_buf = &(*iter);
+		g_name_map.get().insert(new_entry);
 		return buf;
-	}
-	inline NameEntry* get_name_entry(const c8* name)
-	{
-		if (!name)
-		{
-			return nullptr;
-		}
-		return *(((NameEntry**)name) - 1);
 	}
 	LUNA_RUNTIME_API void retain_name(const c8* name)
 	{
 		if (!name) return;
-		LockGuard guard(g_name_mtx);
-		NameEntry* entry = get_name_entry(name);
-		if (!entry) return;
-		if (!entry->m_ref_count)
-		{
-			remove_name_from_cache_list(entry);
-		}
-		++entry->m_ref_count;
+		atom_inc_u32(&(get_name_entry(name)->m_ref_count));
 	}
 	LUNA_RUNTIME_API void release_name(const c8* name)
 	{
 		if(!g_name_inited) return;
 		if (!name) return;
-		LockGuard guard(g_name_mtx);
 		NameEntry* entry = get_name_entry(name);
-		if (!entry) return;
-		--entry->m_ref_count;
-		if (!entry->m_ref_count)
+		u32 r = atom_dec_u32(&(entry->m_ref_count));
+		if (!r)
 		{
-			add_name_to_cache_list(entry);
+			LockGuard guard(g_name_mtx);
+			erase_entry(entry);
 		}
 	}
 	LUNA_RUNTIME_API name_id_t get_name_id(const c8* name)
 	{
 		if (!name) return 0;
-		LockGuard guard(g_name_mtx);
-		NameEntry* entry = get_name_entry(name);
-		return entry->m_id;
+		return get_name_entry(name)->m_id;
 	}
 	LUNA_RUNTIME_API usize get_name_size(const c8* name)
 	{
