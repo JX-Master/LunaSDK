d2072d1af16f0492545965adacf29e3add08d21c
JX-Master <jxmaster@yeah.net>
2023-12-28 13:17:49 +0800

    [ImGui]Copy ImGui header files on installation.

--- Modules/Luna/ImGui/xmake.lua
+++ Modules/Luna/ImGui/xmake.lua
@@ -3,4 +3,16 @@ luna_sdk_module_target("ImGui")
     add_files("Source/**.cpp")
     add_luna_modules("Runtime", "RHI", "HID", "Font", "ShaderCompiler")
     add_packages("imgui", {})
+
+    after_install(function(target)
+        local imgui_package = target:pkg("imgui")
+        local imgui_includedir = imgui_package:installdir()
+        if os.host() == "windows" then 
+            imgui_includedir = path.join(imgui_includedir, "..", "..", "include")
+        elseif os.host() == "macosx" then 
+            imgui_includedir = path.join(imgui_includedir, "include")
+        end
+        print(imgui_includedir)
+        os.cp(path.join(imgui_includedir, "*.h"), path.join(target:installdir(), "include", "Luna", "ImGui"))
+    end)
 target_end()
