341f3a255245e264129b631ed29e49c9533548d7
JX-Master <jxmaster@yeah.net>
2024-03-14 09:40:20 +0800

    [RHI]Add docs.

--- Modules/Luna/RHI/QueryHeap.hpp
+++ Modules/Luna/RHI/QueryHeap.hpp
@@ -14,14 +14,23 @@ namespace Luna
 {
     namespace RHI
     {
+        //! @addtogroup RHI
+        //! @{
+        
+        //! Specifies query type.
         enum class QueryType : u8
         {
+            //! Specifies occlusion query.
             occlusion,
+            //! Specifies timestamp query that works for non-copy queue.
             timestamp,
+            //! Specifies timestamp query that works for copy queue.
             timestamp_copy_queue,
+            //! Specifies pipeline statistics query.
             pipeline_statistics,
         };
 
+        //! Specifies one query in pipeline statistics query heap.
         struct PipelineStatistics
         {
             //! Number of vertex shader invocations.
@@ -36,6 +45,7 @@ namespace Luna
             u64 cs_invocations;
         };
 
+        //! Describes one query heap.
         struct QueryHeapDesc
         {
             //! The type of the query heap.
@@ -49,10 +59,14 @@ namespace Luna
                 count(count) {}
         };
 
+        //! @interface IQueryHeap
+        //! Contains one array of query elements that can be used to query pipeline execution information, such
+        //! as pixel occlusion, execution time and pipeline statistics.
         struct IQueryHeap : virtual IDeviceChild
         {
             luiid("{11c98a1e-1fd4-48c7-828b-96c56239e6ca}");
 
+            //! Gets the descriptor of the query heap.
             virtual QueryHeapDesc get_desc() = 0;
 
             //! Copies timestamp query results from query heap to the user-provided buffer.
@@ -79,5 +93,7 @@ namespace Luna
             //! If this query heap is not `QueryHeapType::pipeline_statistics`, this function fails with `BasicError::not_supported`.
             virtual RV get_pipeline_statistics_values(u32 index, u32 count, PipelineStatistics* values) = 0;
         };
+
+        //! @}
     }
 }
