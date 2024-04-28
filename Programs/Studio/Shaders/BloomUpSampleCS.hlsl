f0a0f462aafa26dc1930aa776a1c86e28059fc05
JX-Master <zhoujixiang1997@hotmail.com>
2024-04-28 12:36:16 +0800

    [Studio]Fix crash on macOS.

--- Programs/Studio/Shaders/BloomUpSampleCS.hlsl
+++ Programs/Studio/Shaders/BloomUpSampleCS.hlsl
@@ -11,18 +11,19 @@ Texture2D<float4> g_bloom_tex : register(t2);
 RWTexture2D<float4> g_dst_tex : register(u3);
 SamplerState g_sampler : register(s4);
 
-float3 filter(Texture2D<float4> tex, int2 pos)
+float3 filter(Texture2D<float4> tex, int2 pos, int2 tex_size)
 {
     float3 color = 0;
+    tex_size -= int2(1, 1);
     color += tex[pos].rgb * 4;
-    color += tex[pos + int2(1, 0)].rgb * 2;
-    color += tex[pos + int2(-1, 0)].rgb * 2;
-    color += tex[pos + int2(0, 1)].rgb * 2;
-    color += tex[pos + int2(0, -1)].rgb * 2;
-    color += tex[pos + int2(1, 1)].rgb;
-    color += tex[pos + int2(1, -1)].rgb;
-    color += tex[pos + int2(-1, 1)].rgb;
-    color += tex[pos + int2(-1, -1)].rgb;
+    color += tex[clamp(pos + int2(1, 0), int2(0, 0), tex_size)].rgb * 2;
+    color += tex[clamp(pos + int2(-1, 0), int2(0, 0), tex_size)].rgb * 2;
+    color += tex[clamp(pos + int2(0, 1), int2(0, 0), tex_size)].rgb * 2;
+    color += tex[clamp(pos + int2(0, -1), int2(0, 0), tex_size)].rgb * 2;
+    color += tex[clamp(pos + int2(1, 1), int2(0, 0), tex_size)].rgb;
+    color += tex[clamp(pos + int2(1, -1), int2(0, 0), tex_size)].rgb;
+    color += tex[clamp(pos + int2(-1, 1), int2(0, 0), tex_size)].rgb;
+    color += tex[clamp(pos + int2(-1, -1), int2(0, 0), tex_size)].rgb;
     return color / 16;
 }
 
@@ -34,7 +35,7 @@ void main(uint3 dispatch_thread_id : SV_DispatchThreadID)
     float2 uv = dst_texel_size * ((float2)dispatch_thread_id.xy + 0.5f);
     //float3 prev_mip = filter(g_upsample_tex, uv, src_texel_size * up_sample_radius);
     //float3 prev_mip = g_upsample_tex.SampleLevel(g_sampler, uv, 0);
-    float3 prev_mip = filter(g_upsample_tex, dispatch_thread_id.xy / 2);
-    float3 current_mip = filter(g_bloom_tex, dispatch_thread_id.xy);
+    float3 prev_mip = filter(g_upsample_tex, dispatch_thread_id.xy / 2, int2(src_tex_width, src_tex_height));
+    float3 current_mip = filter(g_bloom_tex, dispatch_thread_id.xy, int2(dst_tex_width, dst_tex_height));
     g_dst_tex[dispatch_thread_id.xy].rgb = prev_mip + current_mip;
 }
