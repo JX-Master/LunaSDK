6256a3dba0ae8bbb2df72fbd3977d3ddfbe582bf
JX-Master <jxmaster@yeah.net>
2024-07-12 13:24:12 +0800

    Lock volk version.

--- xmake.lua
+++ xmake.lua
@@ -130,7 +130,7 @@ option("rhi_api")
 option_end()
 
 if is_config("rhi_api", "Vulkan") then 
-    add_requires("volk", {configs = {header_only = true}})
+    add_requires("volk 1.3.275+0", {configs = {header_only = true}})
     add_requires("vulkan-memory-allocator")
 elseif is_config("rhi_api", "D3D12") then 
     add_requires("d3d12-memory-allocator", {configs = {toolchains = "msvc"}}) -- currently d3d12-memory-allocator does not support clang-cl.
